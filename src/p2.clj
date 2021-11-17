a58da4fd89cbb10a415b74b12f44b9c4e22aa189
jungwoo <50188177+jungwookim@users.noreply.github.com>
2021-11-17 15:23:41 +0900

    part 1 done

--- src/p2.clj
+++ src/p2.clj
@@ -1,5 +1,6 @@
 (ns p2)
 
+; part 1
 (defn read-input
   "output: [\"abc\" \"abf\" \"bac\"]"
   [path]
@@ -38,8 +39,14 @@
    :y 0
    :z 0})
 
-(defn contains-n-freq-char? [n li]
-  (some #(>= % n) li))
+(defn calc-freq [li]
+  (apply * (reduce (fn [[res1 res2] val]
+                     (prn res1 res2 val)
+                     (cond
+                       (and (some #(== % 3) val) (some #(== % 2) val)) [(inc res1) (inc res2)]
+                       (some #(== % 3) val) [res1 (inc res2)]
+                       (some #(== % 2) val) [(inc res1) res2]
+                       :else [res1 res2])) [0 0] li)))
 
 (defn update-freq [acc-map character]
   (let [key (character-to-keyword character)]
@@ -47,34 +54,17 @@
       (assoc acc-map key (inc (get acc-map key))))))
 
 (defn words-to-freq
-  "return: collections of alphabet map"
+  "return: collections of alphabet map. ex. [{:a 0 :b 1 :c 2 ... :z 0} ...]"
   [words]
   (map (fn [word]
          (let [my-map (get-new-alphabet-map)]
            (reduce update-freq my-map word))) words))
 
-(defn calc2
-  "calc count >= 2 and >=3 cases"
-  [coll]
-  (loop [word-map-seq (seq coll)
-         cnt-gte-2 0
-         cnt-gte-3 0]
-    (let [values (vals (first word-map-seq))]
-      (cond
-        (contains-n-freq-char? 3 values) (recur (rest word-map-seq)
-                                                (inc cnt-gte-2)
-                                                (inc cnt-gte-3))
-        (contains-n-freq-char? 2 values) (recur (rest word-map-seq)
-                                                (inc cnt-gte-2)
-                                                cnt-gte-3)))
-    (* cnt-gte-2 cnt-gte-3)))
+(comment (->> "resources/input_p2.txt"
+              read-input
+              words-to-freq
+              (map (fn [x]
+                     (vals x)))
+              calc-freq))
 
-(comment (-> "resources/input_p2.txt"
-             read-input
-             words-to-freq
-             calc2))
-
-; word 순회하면서 map 업데이트
-; 2개 이상 발견된거 있으면 ans1, ans2 둘다 +1
-; 3개 이상 발견된거 있으면 ans2 +1
-; ans1 * ans2
+; part 2
