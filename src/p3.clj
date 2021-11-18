5ddee50caf7457bb86f152ceb5e02a6624ac990e
jungwoo <50188177+jungwookim@users.noreply.github.com>
2021-11-18 13:52:18 +0900

    day3 part2 done

--- src/p3.clj
+++ src/p3.clj
@@ -6,7 +6,7 @@
   (mapv (fn [x] (mapv (fn [y] (f x y)) (range sx))) (range sy)))
 
 (defn matrix []
-  (vec2d 1500 1500 (constantly -1)))
+  (vec2d 2000 2000 (constantly -1)))
 
 (defn update-cell [matrix id x y]
   (if (neg? (get-in matrix [x y]))
@@ -21,12 +21,12 @@
        flatten
        (partition 3)))
 ;
-;(flatten-vals (map (fn [[a b c d e]] (gen-modified-vals a b c d e)) [[100 1 3 4 4] [101 1 3 4 4]]))
-;
-;(reduce (fn [acc [x y id]]
-;          (update-cell acc id x y))
-;        (matrix)                                            ; 초기값
-;        (flatten-vals (gen-modified-vals 100 1 3 4 4)))
+(flatten-vals (map (fn [[a b c d e]] (gen-modified-vals a b c d e)) [[100 1 3 4 4] [101 1 3 4 4]]))
+
+(reduce (fn [acc [x y id]]
+          (update-cell acc id x y))
+        (matrix)                                            ; 초기값
+        (flatten-vals (gen-modified-vals 100 1 3 4 4)))
 
 (defn logic-part1 [data]
   (reduce (fn [acc [x y id]]
@@ -75,14 +75,37 @@
        (filter #(zero? %))
        count))
 
+; part2
+
+(defn get-total-count-by-id [path]
+  (->> path
+       prepare-data
+       (map (fn [[id _ [sx sy]]] (list id (* sx sy))))))
+
+(defn logic-part2 [path data]
+  (let [current-id-count (->> data
+                              logic-part1
+                              flatten
+                              (filter #(pos? %))
+                              frequencies)
+
+        total-count-by-id (get-total-count-by-id path)]
+    (-> (filter (fn [[x y]] (== y (get current-id-count x -1))) total-count-by-id)
+        first
+        first)))
 (defn solve-part2 [path]
   (->> path
-       prepare-data))
+       prepare-data
+       (logic-part2 path)))
+
+(defn final-matrix-shape [path]
+  (->> path
+       prepare-data
+       logic-part1))
 
 (comment
   (solve-part1 "resources/sample_input_p3.txt"),
   (solve-part1 "resources/input_p3.txt"),
   (solve-part2 "resources/sample_input_p3.txt"),
-  (solve-part2 "resources/input_p3.txt"))
-
-
+  (solve-part2 "resources/input_p3.txt"),
+  (final-matrix-shape "resources/sample_input_p3.txt"))
