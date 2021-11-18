6ab8801624e9f1a785f7f3995701c5e84167782a
jungwoo <50188177+jungwookim@users.noreply.github.com>
2021-11-18 11:29:27 +0900

    day3 part1 ongoing

--- src/p3.clj
+++ src/p3.clj
@@ -6,7 +6,7 @@
   (mapv (fn [x] (mapv (fn [y] (f x y)) (range sx))) (range sy)))
 
 (defn matrix []
-  (vec2d 10 10 (constantly -1))) ;matrix
+  (vec2d 1500 1500 (constantly -1)))
 
 (defn update-cell [matrix id x y]
   (if (neg? (get-in matrix [x y]))
@@ -20,15 +20,62 @@
   (->> values
        flatten
        (partition 3)))
+;
+;(flatten-vals (map (fn [[a b c d e]] (gen-modified-vals a b c d e)) [[100 1 3 4 4] [101 1 3 4 4]]))
+;
+;(reduce (fn [acc [x y id]]
+;          (update-cell acc id x y))
+;        (matrix)                                            ; 초기값
+;        (flatten-vals (gen-modified-vals 100 1 3 4 4)))
 
-(reduce (fn [acc [x y id]]
-          (update-cell acc id x y))
-        (matrix)
-        (flatten-vals (gen-modified-vals 100 1 3 4 4)))
+(defn logic-part1 [data]
+  (reduce (fn [acc [x y id]]
+            (update-cell acc id x y))
+          (matrix)                                          ; 초기값
+          (flatten-vals (map (fn [[id [ix iy] [sx sy]]]
+                               (gen-modified-vals id ix iy sx sy))
+                             data))))
 
 ; read-input and parsing
 
 (defn read-input [path]
+  (-> path
+      slurp
+      (clojure.string/split #"\n")))
+
+(defn parse-id
+  "input: \"#44\", output: 44"
+  [string]
+  (Integer/parseInt (subs string 1 (count string))))
+
+(defn parse-pos
+  "input: \"1,3:\", output: (1 3)"
+  [string]
+  (->> (clojure.string/split (subs string 0 (dec (count string))) #",")
+       (mapv (fn [x] (Integer/parseInt x)))))
+
+(defn parse-size
+  "input: \"4x4\", output: (4 4)"
+  [string]
+  (->> (clojure.string/split string #"x")
+       (mapv (fn [x] (Integer/parseInt x)))))
+
+(defn prepare-data [path]
   (->> path
-       slurp
-       (clojure.string/split #"\n")))
+       read-input
+       (mapv (fn [x] (clojure.string/split x #" ")))
+       (mapv (fn [[id _ pos size]]
+               (list (parse-id id) (parse-pos pos) (parse-size size))))))
+
+(defn solve-part1 [path]
+  (->> path
+       prepare-data
+       logic-part1
+       flatten
+       (filter #(== % 0))
+       count))
+
+(comment
+  (solve-part1 "resources/sample_input_p3.txt"),
+  (solve-part1 "resources/input_p3.txt"),)
+
