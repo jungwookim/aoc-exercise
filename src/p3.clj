2f16c99221c37ce210bccf542c0d7e42ffd641d1
jungwoo <50188177+jungwookim@users.noreply.github.com>
2021-11-19 13:48:16 +0900

    applied p3 reviewed

--- src/p3.clj
+++ src/p3.clj
@@ -22,9 +22,21 @@
 (defn gen-modified-vals
   "output: sequence of (target-x target-y id)"
   [id ix iy sx sy]
-  (map (fn [x] (map (fn [y] (list (+ iy x) (+ ix y) id)) (range sx))) (range sy)))
+  (map (fn [x] (map (fn [y] [(+ iy x) (+ ix y) id])
+                    (range sx)))
+       (range sy)))
+;
+;(for [x (range sx)
+;      y (range sy)]
+;  ((list (+ iy x))))
+; for문으로 바꿀 수 있음 ..!
+; list가 아니라 vector로 반환하는게 낫다
+; list는 데이터를 순차적으로 처리할 때 용이하고 수정 삭제를 할 때 유리한데...
+; 보통은 vector가 유리함.. 관례임 사실
+; 좌표의 특성상.. hash-map이 그래도 좋음
 
-(defn flatten-vals
+; 함수의 이름과 하는 일이 일치하지 않다. 적절한 이름이 찾아지지 않는다면 함수일 필요가 없을 수도
+(defn group-by-data
   "flatten and partition 3"
   [values]
   (->> values
@@ -44,10 +56,32 @@
   (reduce (fn [acc [x y id]]
             (update-cell acc id x y))
           (matrix)                                          ; 초기값
-          (flatten-vals (map (fn [[id [ix iy] [sx sy]]]
-                               (gen-modified-vals id ix iy sx sy))
-                             data))))
+          (group-by-data (map (fn [[id [ix iy] [sx sy]]]
+                                (gen-modified-vals id ix iy sx sy))
+                              data))))
 
+(defn update-data-info [data]
+  (map (fn [[id [ix iy] [sx sy]]]
+         (gen-modified-vals id ix iy sx sy))
+       data))
+
+(defn agg-data-into-matrix [data]
+  (reduce (fn [acc [x y id]]
+            (update-cell acc id x y))
+          (matrix)
+          data))
+
+(defn logic-part1-advanced
+  "update each one"
+  [data]
+  (->> data
+       update-data-info
+       group-by-data
+       agg-data-into-matrix))
+
+
+
+;[[:blank ... 1 1 ...] ...]
 ; read-input and parsing
 
 (defn read-input [path]
@@ -59,6 +93,8 @@
   "input: \"#44\", output: 44"
   [string]
   (Integer/parseInt (subs string 1 (count string))))
+
+; 정규식을 쓰면 더 간단한 수도 있음
 
 (defn parse-pos
   "input: \"1,3:\", output: (1 3)"
@@ -75,18 +111,22 @@
 (defn prepare-data [path]
   (->> path
        read-input
-       (mapv (fn [x] (clojure.string/split x #" ")))
+       (map (fn [x] (clojure.string/split x #" ")))
        (mapv (fn [[id _ pos size]]
                (list (parse-id id) (parse-pos pos) (parse-size size))))))
+; parsing한 결과 현재 list -> map을 쓰는게 편한다 (위치를 기억하기 힘들 수 있음)
+; hash-map이 좀 좋음
+; mapv를 쓰는게 맞는가?
 
 (defn solve-part1 [path]
   (->> path
        prepare-data
        logic-part1
        flatten
-       (filter #(zero? %))
+       (filter zero?)
        count))
 
+; == vs =
 ; part2
 
 (defn get-total-count-by-id [path]
@@ -99,14 +139,15 @@
   (let [current-id-count (->> data
                               logic-part1
                               flatten
-                              (filter #(pos? %))
+                              (filter pos?)
                               frequencies)
 
         total-count-by-id (get-total-count-by-id path)]
-    (-> (filter (fn [[x y]] (== y (get current-id-count x -1))) total-count-by-id)
-        first
-        first)))
+    (->> (filter (fn [[x y]] (= y (get current-id-count x -1))) total-count-by-id)
+        ffirst)))
 
+; threading macro first and last
+; idiomatic rule: -> : hash-map이나 string에 좀 씀 ->> seq에다가 많이 씀
 (defn solve-part2 [path]
   (->> path
        prepare-data
---
+++ src/p3_reviewed.clj
@@ -0,0 +1,132 @@
+(ns p3_reviewed)
+
+; read-input and parsing
+
+(defn read-input [path]
+  (-> path
+      slurp
+      (clojure.string/split #"\n")))
+
+(defn parse-id
+  "input: \"#44\", output: 44"
+  [string]
+  (Integer/parseInt (subs string 1 (count string))))
+
+(defn parse-pos
+  "input: \"1,3:\", output: (1 3)"
+  [string]
+  (->> (clojure.string/split (subs string 0 (dec (count string))) #",")
+       (mapv (fn [x] (Integer/parseInt x)))))
+
+(defn parse-size
+  "input: \"4x4\", output: (4 4)"
+  [string]
+  (->> (clojure.string/split string #"x")
+       (mapv (fn [x] (Integer/parseInt x)))))
+
+(defn prepare-data
+  "output: vector of {:id 4 :pos (1 3) :size (2 4)}"
+  [path]
+  (->> path
+       read-input
+       (map (fn [x] (clojure.string/split x #" ")))
+       (mapv (fn [[id _ pos size]]
+               {:id (parse-id id) :pos (parse-pos pos) :size (parse-size size)}))))
+
+; part 1 main logics
+
+(defn vec2d
+  "2d vectors"
+  [sx sy f]
+  (mapv (fn [x] (mapv (fn [y] (f x y)) (range sx))) (range sy)))
+
+(defn matrix
+  "init matrix"
+  []
+  (vec2d 2000 2000 (constantly :not-visited)))
+
+(defn update-cell
+  "update cell to id at position (x, y) on matrix"
+  [matrix id x y]
+  (let [target (get-in matrix [x y])]
+    (if (and (keyword? target) (= :not-visited target))
+      (update-in matrix [x y] (constantly id))
+      (update-in matrix [x y] (constantly :visited)))))
+
+
+
+(defn matrix-data-to-update-by-id
+  "output: sequence of (target-x target-y id)"
+  [{id      :id
+    [ix iy] :pos
+    [sx sy] :size}]
+  (for [x (range sx)
+        y (range sy)]
+    {:x (+ iy x) :y (+ ix y) :id id}))
+
+(defn prepare-to-update-data [data]
+  (map (fn [m]
+         (matrix-data-to-update-by-id m))
+       data))
+
+(defn agg-data-into-matrix [to-update-data]
+  (reduce (fn [acc {x :x y :y id :id}]
+            (update-cell acc id x y))
+          (matrix)
+          to-update-data))
+
+(defn logic-part1
+  "update each one"
+  [data]
+  (->> data
+       prepare-to-update-data
+       flatten
+       agg-data-into-matrix))
+
+(defn solve-part1 [path]
+  (->> path
+       prepare-data
+       logic-part1
+       flatten
+       (filter #(= :visited %))
+       count))
+
+; part2
+
+(defn get-total-count-by-id [path]
+  (->> path
+       prepare-data
+       (map (fn [{id      :id
+                  _       :pos
+                  [sx sy] :size}] {:id id :total-count (* sx sy)}))))
+
+(defn logic-part2
+  [path data]
+  (let [current-id-count (->> data
+                              logic-part1
+                              flatten
+                              (remove keyword?)
+                              (filter pos?)
+                              frequencies)
+
+        total-count-by-id (get-total-count-by-id path)]
+    (->> (filter (fn [{id :id total-count :total-count}] (= total-count (get current-id-count id))) total-count-by-id)
+         first
+         :id)))
+
+(defn solve-part2 [path]
+  (->> path
+       prepare-data
+       (logic-part2 path)))
+
+(defn final-matrix-shape [path]
+  (->> path
+       prepare-data
+       logic-part1))
+
+(comment
+  (solve-part1 "resources/sample_input_p3.txt"),
+  (solve-part1 "resources/input_p3.txt"),
+  (solve-part2 "resources/sample_input_p3.txt"),
+  (solve-part2 "resources/input_p3.txt"),
+  (final-matrix-shape "resources/sample_input_p3.txt"))
