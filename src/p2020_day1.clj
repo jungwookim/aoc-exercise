b098e8c726429c0d3d16f28acc7baac5f3a31487
jungwoo <50188177+jungwookim@users.noreply.github.com>
2021-11-20 12:08:09 +0900

    2020 day1

---
+++ src/p2020_day1.clj
@@ -0,0 +1,54 @@
+(ns p2020-day1)
+
+(defn read-input [path]
+  (-> path
+      slurp
+      (clojure.string/split #"\n")))
+
+(defn prepare-data [path]
+  (->> path
+       read-input
+       (map (fn [x] (Integer/parseInt x)))))
+
+(defn comb-seq-2 [int-seq]
+  (for [x1 int-seq
+        x2 int-seq
+        :when (= (+ x1 x2) 2020)]
+    (* x1 x2)))
+
+(defn comb-seq-3 [int-seq]
+  (for [x1 int-seq
+        x2 int-seq
+        x3 int-seq
+        :when (= (apply + [x1 x2 x3]) 2020)]
+    (apply * [x1 x2 x3])))
+
+(defn logic-part1 [int-seq]
+  (->> int-seq
+       comb-seq-2
+       set
+       first))
+
+(defn logic-part2 [int-seq]
+  (->> int-seq
+       comb-seq-3
+       set
+       first))
+
+(defn solve-part1 [path]
+  (->> path
+       prepare-data
+       logic-part1))
+
+(defn solve-part2 [path]
+  (->> path
+       prepare-data
+       logic-part2))
+
+(def sample_input "resources/sample_input_p2020_day1.txt")
+(def full_input "resources/input_p2020_day1.txt")
+(comment
+  (solve-part1 sample_input),
+  (solve-part2 sample_input),
+  (solve-part1 full_input)
+  (solve-part2 full_input))
