43179499a5d53b030199b4064c4416fae27f6673
jungwoo <50188177+jungwookim@users.noreply.github.com>
2021-11-22 15:56:53 +0900

    day4 done

--- src/p2018_day4.clj
+++ src/p2018_day4.clj
@@ -48,11 +48,14 @@
 (defn init-sleep-data [m-seq]
   (->> m-seq
        get-all-guard-ids
-       (map (fn [guard-id]
-              (map (fn [x]
-                     {(keyword guard-id) {(keyword (str x)) 0}})
-                   (range 60))))
-       flatten))
+       (reduce (fn [acc guard-id]
+                 (assoc acc (keyword guard-id) (reduce (fn [acc x]
+                                                         (assoc acc (keyword (str x)) 0))
+                                                       {}
+                                                       (range 60))))
+               {})))
+
+
 
 (defn init-guard-working-time [m-seq]
   (->> m-seq
@@ -61,11 +64,18 @@
                  (assoc acc (keyword guard-id) []))
                {})))
 
-
+(defn map-vals-flatten
+  "input: {:99 [40 41 42 43 44 45 46 47 48 49]}"
+  [m]
+  (let [guard-id (first (keys m))
+        minutes (first (vals m))]
+    (map (fn [min]
+           {(keyword guard-id) min})
+         minutes)))
 (defn interpolate-data [m-seq]
   (->> (reduce (fn [{acc-data      :acc-data
                      current-guard :current-guard}
-                    {action :action
+                    {_ :action
                      min    :min
                      who    :who}]
                  (if (nil? who)
@@ -76,39 +86,70 @@
                {:acc-data      (init-guard-working-time m-seq)
                 :current-guard nil}
                m-seq)
-       :acc-data))
+       :acc-data
+       (map (fn [[guard-id times]]
+              (let [x (into [] (partition 2 times))]
+                (map (fn [[start end]] {(keyword guard-id) (range start end)}) x))))
+       flatten
+       (map (fn [x] (map-vals-flatten x)))
+       flatten))
 
 
-(defn update-data [data who min]
-  {:acc-data      (assoc-in data
-                            [(keyword who) (keyword min)]
-                            (inc (get-in
-                                   data
-                                   [(keyword who) (keyword min)])))
-   :current-guard who})
+(defn update-data [data val]
+      (let [guard-id (keyword (first (keys val)))
+            min (keyword (str (first (vals val))))]
+        (assoc-in data
+                  [guard-id min]
+                  (inc (get-in
+                         data
+                         [guard-id min])))))
+
+
+(defn common-logic [path]
+  (let [prepared-data (->> path
+                           prepare-data
+                           interpolate-data)
+        init-data (->> path
+                       prepare-data
+                       init-sleep-data)]
+    (reduce (fn [acc-data
+                 val]
+              (update-data acc-data val))
+            init-data
+            prepared-data)))
+
+(defn total-sleep-time [minutes]
+  (apply + minutes))
+
+(defn get-most-sleep-min [m]
+  (apply max-key val m))
 
 (defn solve-part1 [path]
-  (let [prepared-data (->> path
-                           prepare-data)]
-    (let [init-data (->> prepared-data
-                         init-sleep-data)]
-      (reduce (fn [{acc-data      :acc-data
-                    current-guard :current-guard}
-                   {action :action
-                    min    :min
-                    who    :who}]
-                (if (nil? who)
-                  (update-data acc-data current-guard min)
-                  (update-data acc-data who min)))
-              {:acc-data init-data :current-guard nil}
-              prepared-data))))
+ (->> path
+      common-logic
+      (map (fn [x] {:guard-id (name (first x))
+                    :total-sleep-time (total-sleep-time (vals (second x)))
+                    :get-most-sleep-min (get-most-sleep-min (second x))}))
+      (apply max-key :total-sleep-time)
+      (apply (fn [x _ z] (* (Integer/parseInt (second x))
+                            (Integer/parseInt (name (first (second z)))))))))
 
+
+(defn solve-part2 [path]
+  (->> path
+       common-logic
+       (map (fn [x] {:guard-id (name (first x))
+                     :total-sleep-time (total-sleep-time (vals (second x)))
+                     :get-most-sleep-min (get-most-sleep-min (second x))}))
+       (apply max-key #(second (:get-most-sleep-min %)))
+       (apply (fn [x _ z] (* (Integer/parseInt (second x))
+                             (Integer/parseInt (name (first (second z)))))))))
 
 (def sample-input-path "resources/sample_input_p4.txt")
 (def input-path "resources/input_p4.txt")
 
 (comment
-  (prepare-data sample-input-path),
-  (interpolate-data (prepare-data sample-input-path)),
-  (solve-part1 sample-input-path),)
-; part 1
+  (solve-part1 sample-input-path),
+  (solve-part2 sample-input-path),
+  (solve-part1 input-path),
+  (solve-part2 input-path),)
