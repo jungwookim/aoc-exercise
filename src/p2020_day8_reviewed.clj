49cff96cc50dfbfbd52e3337765f65a646f0aaac
jungwoo <50188177+jungwookim@users.noreply.github.com>
2021-11-30 12:02:28 +0900

    defmulti and defmethod for p8

--- src/p2020_day8_reviewed.clj
+++ src/p2020_day8_reviewed.clj
@@ -1,8 +1,5 @@
-(ns p2020_day8
+(ns p2020_day8_reviewed
   (:require [clojure.string :as s]))
-
-; vector로 index 접근을 계속 하도록 하자
-; visited vector 를 관리해서 이미 방문한 인덱스가 발견되면 loop를 종료하고 acc 값을 출력하자
 
 (def sample-input-path "resources/sample_input_p2020_day8.txt")
 (def input-path "resources/input_p2020_day8.txt")
@@ -25,31 +22,28 @@
    :break1? false
    :break2? false})
 
-; processing의 결과는 현재 인덱스와 현재 visited set, acc-val을 관리하도록 하자
-; processing을 한번 거치면 update val, update idx, update visited vector를 하자
+(defmulti update-state (fn [param] (:action param)))
 
-(defn update-in-case-of-nop [state idx]
+(defmethod update-state "nop" [{:keys [state idx]}]
   (-> state
       (update :visited #(conj % idx))
       (update :idx inc)))
 
-(defn update-in-case-of-acc [state idx value]
+(defmethod update-state "acc" [{:keys [state idx value]}]
   (-> state
       (update :acc-val #(+ % value))
       (update :visited #(conj % idx))
       (update :idx inc)))
 
-(defn update-in-case-of-jmp [state idx value]
+(defmethod update-state "jmp" [{:keys [state idx value]}]
   (-> state
       (update :visited #(conj % idx))
       (update :idx #(+ % value))))
 
-; 반대로 하자
 (defn index-out-of-bounds [v-size idx]
   (or (< (dec v-size) idx)
       (neg? idx)))
 
-; 반대로 하자
 (defn break-point-part1? [state]
   (->> (frequencies (:visited state))
        (some (fn [[_ v]] (> v 1)))))
@@ -70,10 +64,7 @@
   "state: [[\"nop\" \"+0\"] [\"acc\" \"+1\"] ... ]\n"
   [data {:keys [idx] :as state}]
   (let [[action value] (nth data idx)]
-    (-> (case action
-          "nop" (update-in-case-of-nop state idx)
-          "acc" (update-in-case-of-acc state idx value)
-          "jmp" (update-in-case-of-jmp state idx value))
+    (-> (update-state {:action action :state state :idx idx :value value})
         (update-state-break1)
         (update-state-break2 (count data)))))
 
@@ -91,25 +82,22 @@
          first
          (agg-ans data))))
 
-; defnmulti로 변경해보기
-
 (defn generate-candidate-data [data]
   (->> data
        (map-indexed (fn [idx [action value]]
                       (case action
-                        "nop" (update data idx (constantly ["jmp" value])) ; assoc data idx "jmp" value
+                        "nop" (update data idx (constantly ["jmp" value]))
                         "jmp" (update data idx (constantly ["nop" value]))
                         "acc" nil)))
        (remove nil?)))
 
 (defn logic-part2 [data]
-    (->> init-state
-         (iterate #(processing data %))
-         (drop-while #(and (not (:break2? %))
-                          (not (:break1? %))))
-         first))
+  (->> init-state
+       (iterate #(processing data %))
+       (drop-while #(and (not (:break2? %))
+                       (not (:break1? %))))
+       first))
 
-; take-while을 쓰면 좋을 수도 있음
 
 (defn solve-part2 [path]
   (->> (parsing path)
