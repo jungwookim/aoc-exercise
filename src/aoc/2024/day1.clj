2909b2d8170181b4a0498180ae3c2bf542870d83
jungwookim <kjw0323@gmail.com>
2024-12-01 21:58:31 +0900

    aoc 2024-day1

--- src/aoc/2024/day1.clj
+++ src/aoc/2024/day1.clj
@@ -5,37 +5,43 @@
 (defn parse [input]
   (mapv #(Integer/parseInt %) (s/split input #"\s+")))
 
+(defn prepare [path]
+  (let [input (->> path
+                  (u/read-input)
+                  (map parse))]
+    [(->> input
+          (map first)
+          sort)
+     (->> input
+          (map second)
+          sort)]))
+
 (defn part1 [path]
-  (let [input (u/read-input path)
-        first_array (->> input
-                         (map parse)
-                         (map first)
-                         sort)
-        second_array (->> input
-                          (map parse)
-                          (map second)
-                          sort)]
-    (apply + (map (fn [x y]
-                    (abs (- x y)))
-                  first_array
-                  second_array))))
+  (let [[first_list second_list] (prepare path)]
+    (apply + (map #(abs (- %1 %2))
+                  first_list
+                  second_list))))
 
 (defn part2 [path]
-  (let [input (u/read-input path)
-        first_array (->> input
-                         (map parse)
-                         (map first)
-                         sort)
-        second_array (->> input
-                          (map parse)
-                          (map second)
-                          sort)]
-    (apply + (map (fn [x]
-                    (* x (count (filter #(= x %) second_array))))
-                  first_array))))
+  (let [[first_list second_list] (prepare path)
+        freq (frequencies second_list)]
+    (apply + (map #(* %1 (get freq %1 0))
+                  first_list))))
 
 (comment
+  ;; Today I learned
+  (def input (->> (str "[" (slurp "resources/2024/day1_part1.txt") "]")
+                  read-string
+                  (partition 2)
+                  (apply map vector)))
+
+  (defn part1b [[l r]]
+    (apply + (mapv (comp abs -) (sort l) (sort r))))
+
+  (part1b input)
+
   (def sample-input-path "resources/2024/day1_part1.txt")
 
   (part1 sample-input-path)
-  (part2 sample-input-path))
+  (part2 sample-input-path)
+  )
