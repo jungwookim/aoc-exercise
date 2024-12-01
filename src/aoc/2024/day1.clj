79213a4dc4f35113ef340777f2ee9c13a54f0463
jungwookim <kjw0323@gmail.com>
2024-12-01 14:31:12 +0900

    aoc 2024-day1

--- src/aoc/2024/day1.clj
+++ src/aoc/2024/day1.clj
@@ -1,6 +1,41 @@
 (ns aoc.2024.day1
-  (:require [util.core :as u]))
+  (:require [util.core :as u]
+            [clojure.string :as s]))
+
+(defn parse [input]
+  (mapv #(Integer/parseInt %) (s/split input #"\s+")))
+
+(defn part1 [path]
+  (let [input (u/read-input path)
+        first_array (->> input
+                         (map parse)
+                         (map first)
+                         sort)
+        second_array (->> input
+                          (map parse)
+                          (map second)
+                          sort)]
+    (apply + (map (fn [x y]
+                    (abs (- x y)))
+                  first_array
+                  second_array))))
+
+(defn part2 [path]
+  (let [input (u/read-input path)
+        first_array (->> input
+                         (map parse)
+                         (map first)
+                         sort)
+        second_array (->> input
+                          (map parse)
+                          (map second)
+                          sort)]
+    (apply + (map (fn [x]
+                    (* x (count (filter #(= x %) second_array))))
+                  first_array))))
 
 (comment
-  "Getting started with Advent of Code 2024, day 1."
-  (u/read-input "resources/2024/day1_part1.txt"))
+  (def sample-input-path "resources/2024/day1_part1.txt")
+
+  (part1 sample-input-path)
+  (part2 sample-input-path))
